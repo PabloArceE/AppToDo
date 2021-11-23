e5c02b16eec2bffa68ffd6f2669209916f569578
PabloArceE <arcepablo07@gmail.com>
2021-11-22 21:27:27 -0300

    local storage

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -5,20 +5,21 @@ export class TodoList{
 
     constructor(){
 
-        this.arrTodos = [];
-
+        //this.arrTodos = [];
+        this.cargarLocalStorage();
     }
 
     // agregar una tarea .push()
     nuevoTodo(todo){
         this.arrTodos.push(todo);
+        this.guardarLocalStorage();
     }
 
     // eliminar todo
     eliminarTodo(id){
 
-       this.arrTodos = this.arrTodos.filter( todo => todo.id != id); // se filtra el todo con el id pasado como argumeto y se lo asigna al array 
-
+        this.arrTodos = this.arrTodos.filter( todo => todo.id != id); // se filtra el todo con el id pasado como argumeto y se lo asigna al array 
+        this.guardarLocalStorage();
     }
 
     // marcar todo como completado
@@ -27,6 +28,7 @@ export class TodoList{
         for(const todo of this.arrTodos){
             if(todo.id == id){
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -37,7 +39,20 @@ export class TodoList{
     // en este método se le aplica un filtro al array arrTodos que devuelve en un nuevo array los elementos que no estan completados y lo asigna al arrTodos, dejando de lado de esta manera, los elementos que ya han sido completados. En otras palabras elimina del arreglo los elementos que han sido completados. 
     eliminarCompletados(){
         this.arrTodos = this.arrTodos.filter( todo => !todo.completado);
+        this.guardarLocalStorage();
     }
 
+
+    guardarLocalStorage(){
+        localStorage.setItem('todo', JSON.stringify(this.arrTodos)); 
+    }
+
+    cargarLocalStorage(){
+
+    this.arrTodos = (localStorage.getItem('todo')) 
+                  ? this.arrTodos = JSON.parse(localStorage.getItem('todo')) 
+                  : [];
+
+    }
 
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,5 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-const tarea1 = new Todo('aprender JavaScript');
-
-todoList.nuevoTodo(tarea1);
-
-console.log(todoList);
-
-crearTodoHtml(tarea1);
- 
+todoList.arrTodos.forEach(crearTodoHtml);
 
